826db8cb70313d265506f49f898e0f0987239b62
Hidayet Eren Uludağ <76475118+hidayetErenUludag@users.noreply.github.com>
2025-05-19 00:01:37 +0300

    Added some customer features
    Cart and rating can have mismatch info since the restaurant is entered manual
    Ratings output format ??

--- Main.py
+++ Main.py
@@ -1,8 +1,14 @@
 from Connector import Connector
 from Service.Customer_Service import Customer_Service
 
-connector = Connector('root', 'password', 'CS202')
+connector = Connector('root', 'test123', 'cs202')
 customer_service = Customer_Service(connector)
-result = customer_service.select_by_id(id=1)
 
+result1 = customer_service.select_by_id(id="3")
+result = customer_service.login(username='customer1',password='cust1pass')
+result2 = customer_service.get_ratings(customer_id="14")
+res1 = customer_service.create_rating(cart_id="5", rating="5", comment="Excelent", customer_id="14", restaurant_id="5")
+print(res1)
+print(result1)
 print(result)
+print(result2)
--- Service/Customer_Service.py
+++ Service/Customer_Service.py
@@ -1,8 +1,59 @@
-class Customer_Service(): 
-    def __init__(self, connection):
-        self.connection = connection 
+from Entity.User import User
+from Entity.User.Customer import Customer
 
-    def select_by_id(self, id): 
+
+class Customer_Service():
+    def __init__(self, connection):
+        self.connection = connection
+        self.user = None
+
+    def login(self, username, password):
+        try:
+            query = (f"SELECT user_id FROM User U JOIN Customer C ON C.customer_id = "
+                     f"U.user_id WHERE user_name = '{username}' AND password = '{password}'")
+            result = self.connection.execute_query(query)
+            if result and len(result) > 0:
+                id = result[0][0]  # first row, first column = user_id
+                self.user = Customer(id, username, password)
+                return result
+            return 0
+        except Exception as e:
+            print("Login failed")
+            print(e)
+
+    def get_ratings(self, customer_id):
+        # Çıktı düzenlenmeli
+        try:
+            query = (f"SELECT R.id, R.rating, R.comment, R.created_at, Res.restaurant_name "
+                     f"FROM Rating R "
+                     f"JOIN Restaurant Res ON R.restaurant_id = Res.restaurant_id "
+                     f"WHERE R.customer_id = {customer_id} "
+                     f"ORDER BY R.created_at DESC")
+            result = self.connection.execute_query(query)
+            return result
+        except Exception as e:
+            print("Failed to get ratings")
+            print(e)
+            return None
+
+    def create_rating(self, cart_id, rating, comment, customer_id, restaurant_id):
+        #Should the restaurant Id insertion handled here and should we allow multiple ratings
+        #Cart and rating can have mismatch info since the restaurant is  manual
+        try:
+            query = f"""
+                INSERT INTO Rating (cart_id, rating, comment, customer_id, restaurant_id)
+                VALUES ({cart_id}, {rating}, '{comment}', {customer_id}, {restaurant_id})
+            """
+            self.connection.execute_query(query)
+            print("Created rating")
+            return True
+        except Exception as e:
+            print("Error while creating rating:", e)
+            return e
+
+    def select_by_id(self, id):
         query = f'SELECT * FROM User WHERE user_id= {id}'
         result = self.connection.execute_query(query)
-        return result 
+        return result
+
+
